49e321bdf34622675ed21191be5fe544ed518ec5
Kaushal Patel <krpatel21@outlook.com>
2017-11-18 17:25:30 -0600

    Refactor criteria.

--- src/main/java/misty/crm/controller/HomeController.java
+++ src/main/java/misty/crm/controller/HomeController.java
@@ -1,6 +1,8 @@
 package misty.crm.controller;
 
 import misty.crm.model.User;
+import misty.crm.model.Worker;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
 
 @Controller
@@ -20,9 +24,18 @@ public class HomeController {
     @SuppressWarnings("unchecked")
     public String home(Model model) {
         Session session= sessionFactory.openSession();
-        List<User> users = session.createCriteria(User.class).list();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+
+        CriteriaQuery<User> criteria = builder.createQuery(User.class);
+        criteria.from(User.class);
+        List<User> users = session.createQuery(criteria).getResultList();
+
+        CriteriaQuery<Worker> criteria2 = builder.createQuery(Worker.class);
+        criteria2.from(Worker.class);
+        List<Worker> workers = session.createQuery(criteria2).getResultList();
 
         model.addAttribute("users", users);
+        model.addAttribute("workers", workers);
         return "home";
     }
 }
---
+++ src/main/java/misty/crm/model/Worker.java
@@ -0,0 +1,34 @@
+package misty.crm.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Worker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private BigDecimal amount;
+
+    public Worker() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
